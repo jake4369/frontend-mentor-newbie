6f005f41a082a5f8c126762a923a5125e7cdab1d
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-03-24 23:35:05 +0000

    Add rating state and handleSelectRating function

--- interactive-rating-component/src/App.js
+++ interactive-rating-component/src/App.js
@@ -1,9 +1,17 @@
+import { useState } from "react";
+
 import Rating from "./components/Rating";
 
 const App = () => {
+  const [rating, setRating] = useState(0);
+
+  const handleSelectRating = (value) => {
+    setRating(value);
+  };
+
   return (
     <main className="app">
-      <Rating />
+      <Rating onSelectRating={handleSelectRating} />
     </main>
   );
 };
