13477b4753adaeb3fa609a4305c9fb625c296cc4
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-03-25 00:40:20 +0000

    App.js

--- interactive-rating-component/src/App.js
+++ interactive-rating-component/src/App.js
@@ -1,17 +1,30 @@
 import { useState } from "react";
 
 import Rating from "./components/Rating";
+import Confirmation from "./components/Confirmation";
 
 const App = () => {
   const [rating, setRating] = useState(0);
+  const [isRated, setIsRated] = useState(false);
 
   const handleSelectRating = (value) => {
     setRating(value);
   };
 
+  const handleSubmit = () => {
+    if (rating) setIsRated(true);
+  };
+
   return (
     <main className="app">
-      <Rating onSelectRating={handleSelectRating} />
+      {!isRated ? (
+        <Rating
+          onSelectRating={handleSelectRating}
+          onHandleSubmit={handleSubmit}
+        />
+      ) : (
+        <Confirmation rating={rating} />
+      )}
     </main>
   );
 };
