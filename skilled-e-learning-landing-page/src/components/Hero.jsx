371d5f7efaefc3bfe77eacc4c8c518a3467558da
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-03-27 23:32:00 +0000

    Hero section

---
+++ skilled-e-learning-landing-page/src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import Btn from "./Btn";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <h1>Maximize skill, minimize budget</h1>
+
+      <p>
+        Our modern courses across a range of in-demand skills will give you the
+        knowledge you need to live the life you want.
+      </p>
+
+      <Btn className="btn__hero" />
+    </section>
+  );
+};
+
+export default Hero;
