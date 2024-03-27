01b399b5d464587d59d8e84e77b27651fec7d0d7
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-03-27 21:52:12 +0000

    Header

---
+++ skilled-e-learning-landing-page/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Btn from "./Btn";
+
+const Header = () => {
+  return (
+    <header>
+      <img src="/assets/logo-dark.svg" alt="Skilled" className="logo__header" />
+
+      <Btn className="btn__header" />
+    </header>
+  );
+};
+
+export default Header;
